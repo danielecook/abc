fdc5d3f32c64d6f0d2c0485b1b056f36d367f931
Daniel Cook <danielecook@gmail.com>
2015-09-14 17:58:48 -0500

    create data directory prior to download

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
+mkdir data
 curl ftp://ftp.ncbi.nlm.nih.gov/genomes/Bacteria/all.fna.tar.gz > data/all.fa.gz
 curl ftp://ftp.ncbi.nih.gov/genomes/Bacteria/summary.txt > bacterial_genomes.txt
 
 # Fix contig (genome) names and construct a blast database
-gzcat reference/all.fa.gz | awk '$0 ~ ">" { gsub("^[^>]+","",$0); print $0  } $0 !~ ">" { print }' | makeblastdb -in - -dbtype=nucl -out=data/all_bacteria -title=all_bacteria
+gzcat reference/all.fa | awk '$0 ~ ">" { gsub("^[^>]+","",$0); print $0  } $0 !~ ">" { print }' | makeblastdb -in - -dbtype=nucl -out=data/all_bacteria -title=all_bacteria
