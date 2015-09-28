75cdb58f568f31162aaa5da91f63053a611d098d
Kari Tanaka <kari@dhcp-10-105-64-100.wireless.northwestern.private>
2015-09-28 16:02:12 -0500

    fixed setup blastdb

--- setup.sh
+++ setup.sh
@@ -5,4 +5,6 @@ curl ftp://ftp.ncbi.nlm.nih.gov/genomes/Bacteria/all.fna.tar.gz > reference/all.
 curl ftp://ftp.ncbi.nih.gov/genomes/Bacteria/summary.txt > bacterial_genomes.txt
 
 # Fix contig (genome) names and construct a blast database
-gzcat reference/all.fa.gz | awk '$0 ~ ">" { gsub("^[^>]+","",$0); print $0  } $0 !~ ">" { print }' | makeblastdb -in - -dbtype=nucl -out=reference/all_bacteria -title=all_bacteria
+gzcat reference/all.fa.gz | \
+awk '$0 ~ ">" { gsub("^[^>]+","",$0); print $0  } $0 !~ ">" { print }' | \
+makeblastdb -in - -dbtype=nucl -out=reference/all_bacteria -title=all_bacteria
